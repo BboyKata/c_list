3f590d6eabc814db38829b939b3a663598b6aa9d
Bboy Kata <72092989+BboyKata@users.noreply.github.com>
2024-05-03 23:13:42 +0200

    mod

--- main.c
+++ main.c
@@ -6,6 +6,18 @@ struct entry{
 	struct entry* next;
 };
 
+
+void printList(struct entry* head);
+struct entry* newEntry(int value);
+struct entry* addRight(int value,struct entry* head);
+struct entry* addLeft(int value, struct entry* head);
+struct entry* addMiddle(int value, struct entry* head, int pos);
+struct entry* popRight(struct entry* head);
+struct entry* popLeft(struct entry* head);
+int listLen(struct entry* head);
+
+
+
 void printList(struct entry* head){
 	if(head != NULL){
 		struct entry* cursor = head;
@@ -52,11 +64,11 @@ struct entry* addMiddle(int value, struct entry* head, int pos){
 	/* possibili posizioni:
 	0 |le| 1 |le| 2 |le| 3 |le| 4	
 	*/
-	if(pos>listLen(list)){
+	if(pos>listLen(head)){
 		printf("Index out of bound!\n");
 		return head;
-	}else if(pos == 0) head = addLeft(value,list);
-	else if(pos == listLen(list)) head = addRight(value,list);
+	}else if(pos == 0) head = addLeft(value,head);
+	else if(pos == listLen(head)) head = addRight(value,head);
 	else{
 		struct entry* cursor = head;
 		struct entry* new = newEntry(value);
@@ -111,6 +123,10 @@ int main(void){
 	printf("%i\n",listLen(list));
 	list = popRight(list);
 	list = popLeft(list);
+	printList(list);
+	list = addMiddle(777,list,0);
+	list = addMiddle(888,list,listLen(list));
+	list = addMiddle(999,list,2);
 	printList(list);
 return 0;
 }
